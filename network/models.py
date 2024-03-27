2363384eb4f0d7717f3d9164e4ae417dee301770
Mihael <micostrnad@gmail.com>
2024-03-27 13:47:59 +0100

    Initial commit 2

--- network/models.py
+++ network/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
+# add additional models to represent details about posts, likes, and followers.
+# python manage.py makemigrations
+# python manage.py migrate 
 class User(AbstractUser):
     pass
